bdc8183fc2c098c3fad7e910dfb1d5cc890d9466
Umar Irale <mailpersonal725@gmail.com>
2024-06-24 04:30:22 +0100

    ..........

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,45 @@
-const Button = ({onClick, children, type, disabled, className, ...props}) => {
+const Button = ({
+  children,
+  onClick,
+  type = "button",
+  variant = "primary",
+  size = "medium",
+  disabled = "",
+  className = "",
+}) => {
+  const baseStyles =
+    "border font-bold  focus:outline-none focus:ring-2 focus:ring-opacity-50";
+
+  const variants = {
+    primary: "bg-blue-500 hover:bg-blue-600 focus:ring-slate-500 text-white",
+    secondary:
+      "bg-gray-200 hover:invert focus:ring-gray-500 text-gray-800",
+    danger: "bg-red-500 hover:bg-red-600 focus:ring-red-500 text-white",
+  };
+
+  const sizes = {
+    small: "px-2 py-1 text-sm",
+    medium: "px-2 py-2",
+    large: "px-6 py-3 text-lg",
+  };
+
+  const buttonStyles = `
+  ${baseStyles}
+  ${variants[variant]}
+  ${sizes[size]}
+  ${disabled ? "opacity-50 cursor-not-allowed bg-indigo" : ""}
+  ${className}
+`;
   return (
     <button
-    type={type}
-    className={`button ${className}`}
-    onClick={onClick}
-    disabled={disabled}
-    {...props}
-  >
-    {children}
-  </button>
-  )
-}
+      type={type}
+      className={buttonStyles}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,75 @@
-import { useEffect } from "react";
-import homeBanner from "../assets/Rectangle 6.png";
+import { useEffect, useState } from "react";
+import homeBanner0 from "../assets/Rectangle 6.png";
+import homeBanner1 from "../assets/Rectangle 6.png"
 import rightArrow from "../assets/Right Arrow.png";
+import Button from "../components/Button";
+import leftArrow from "../assets/Left Arrow.png";
 
-// page title manupulation
 function useDocumentTitle(title) {
   useEffect(() => {
     document.title = title;
   }, [title]);
 }
 
+
+const images = [homeBanner0 , homeBanner1] ;
+
 const Home = () => {
-  useDocumentTitle("Welcome to Property")
+  useDocumentTitle("Welcome to Property");
+
+  const [carouselCount, setCarouselCount] = useState(1);
+
+  const prev = () => {
+    setCarouselCount((prevCount) => Math.max(prevCount - 1, 1));
+  };
+
+  const next = () => {
+    setCarouselCount((prevCount) => Math.min(prevCount + 1, 2));
+  };
+
+  const isPrevDisabled = carouselCount === 1;
+  const isNextDisabled = carouselCount === 2;
+
+  const currentImage = images[carouselCount - 1];
+
   return (
     <main className="px-[20px] lg:px-[75px]">
       <section className="w-full h-full flex flex-col lg:flex-row justify-between">
         <article className="lg:pl-[100px] lg:pt-[40%]">
           <h3 className="text-[64px] text-[#9ca3af] font-thin">Project</h3>
           <h4 className="text-[30px] font-bold">Lorum</h4>
+          <div className="controller mt-[20px] flex gap-4">
+            <Button
+              variant="secondary"
+              size="medium"
+              onClick={prev}
+              disabled={isPrevDisabled}
+            >
+              <img src={leftArrow} alt="Previous" className="w-4" />
+            </Button>
+            <Button
+              variant="secondary"
+              size="medium"
+              onClick={next}
+              disabled={isNextDisabled}
+            >
+              <img src={rightArrow} alt="Next" className="w-4" />
+            </Button>
+          </div>
+          <div className="mt-[50px] text-3xl font-thin">
+            0{carouselCount} / 02
+          </div>
         </article>
-        <div className="relative">
-          <img src={homeBanner} alt="homeBanner" />
-          <div className="flex justify-center gap-4 absolute bottom-0 left-0  bg-[#e5e5e5] w-[fit-content] pl-4 pr-[40px] py-[10px] text-[#333]">
-            View Projects <img src={rightArrow} alt="" className="w-[20px]" />
+        <div className="relative transition-opacity duration-500 ease-in-out">
+          <img src={currentImage} alt="homeBanner" />
+          <div className="flex justify-center gap-4 absolute bottom-0 left-0  bg-[#e5e5e5] w-[fit-content] pl-4 pr-[40px] py-[10px] text-[#333] transition-opacity duration-500 ease-in-out hover:cursor-pointer">
+            View Projects{" "}
+            <img src={rightArrow} alt="Arrow" className="w-[20px]" />
           </div>
         </div>
       </section>
     </main>
   );
 };
+
 export default Home;
